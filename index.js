ebdc97ca6ff0ca0c7cf722a3f46904a31b5e6da5
agwadan <agwa.dan@gmail.com>
2023-08-02 17:04:34 +0300

    Loop through forecast dates

--- index.js
+++ index.js
@@ -7,16 +7,34 @@ const fetchData = async (event) => {
   const weatherResultDiv = document.getElementById('weather-result');
 
   try {
-    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=ba82869c357c4038ab682813230108&q=${location}`);
-    const data = await res.json();
-    const filteredData = filterResponse(data);
+    const resCurrent = await fetch(`http://api.weatherapi.com/v1/current.json?key=ba82869c357c4038ab682813230108&q=${location}`);
+    const dataCurrent = await resCurrent.json();
 
+    
+    const resForecast = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ba82869c357c4038ab682813230108&q=${location}&days=3`);
+    const dataForecast = await resForecast.json();
+    
+
+    const filteredData = filterResponse(dataCurrent, dataForecast);
+
+
+    console.log(filteredData);
+    const forecastHTML = filteredData.forecast.map(forecast => `
+    <div class='forecast'>
+      <h3>Date: ${forecast.date}</h3>
+      <p>${forecast.condition}</p>
+      <img src='${forecast.imgUrl}'/>
+    </div>
+    `).join('');
     
       weatherResultDiv.innerHTML = 
      `
         <h2>${filteredData.location}</h2>
         <p>Temperature: ${filteredData.temperature}</p>
-        <p>Humidity: ${filteredData.humidity}</p>
+        <p>Condition: ${filteredData.condition}</p>
+        <img src= '${filteredData.imgUrl}'/>
+
+        ${forecastHTML}
     `; 
   } catch (error) {
     console.error('Error:', error);
@@ -24,12 +42,31 @@ const fetchData = async (event) => {
   }
 }
 
-const filterResponse = (data) => {
+const filterResponse = (dataCurrent, dataForecast) => {
   
   filteredData = {
-    location: `${data.location.country} , ${data.location.name}`,
-    temperature: data.current.feelslike_c,
-    humidity: data.current.humidity
+    location: `${dataCurrent.location.country} , ${dataCurrent.location.name}`,
+    temperature: dataCurrent.current.feelslike_c,
+    condition: dataCurrent.current.condition.text,
+    imgUrl: dataCurrent.current.condition.icon,
+    forecast: [
+      {
+        date: dataForecast.forecast.forecastday[0].date,
+        condition: dataForecast.forecast.forecastday[0].day.condition.text,
+        imgUrl: dataForecast.forecast.forecastday[0].day.condition.icon
+      },
+      {
+        date: dataForecast.forecast.forecastday[1].date,
+        condition: dataForecast.forecast.forecastday[1].day.text,
+        imgUrl: dataForecast.forecast.forecastday[1].day.condition.icon
+      },
+      {
+        date: dataForecast.forecast.forecastday[2].date,
+        condition: dataForecast.forecast.forecastday[2].day.text,
+        imgUrl: dataForecast.forecast.forecastday[2].day.condition.icon
+      }
+      
+    ]
   }
   //console.log(filteredData);
   return filteredData;
